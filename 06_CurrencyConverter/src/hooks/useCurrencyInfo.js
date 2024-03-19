74cfd20b3f326c289e5992c84d2172e883f90e4d
Divyansh Mishra <88049230+divyanshmishra321@users.noreply.github.com>
2024-03-19 15:59:05 +0530

    Currency Converter

---
+++ 06_CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+// Custom hook to fetch data and return the same data using fetch api
+
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
